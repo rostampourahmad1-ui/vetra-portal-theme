eeeb03407f807466949525fd2e28e66458143e5d
rostampourahmad1-ui <3.0789208e+08+rostampourahmad1-ui@users.noreply.github.com>
2026-09-25 20:01:52 +0000

    Prepare Vetra Portal 3.4.0 release

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'VETRA_PORTAL_VERSION', '3.3.0' );
+define( 'VETRA_PORTAL_VERSION', '3.4.0' );
 define( 'VETRA_PORTAL_DIR', get_template_directory() );
 define( 'VETRA_PORTAL_URI', get_template_directory_uri() );
 
